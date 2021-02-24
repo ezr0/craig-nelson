bb5a2a4ee739f3b5e4597e212bb08744a9142c8b
Craig <craignelson88@googlemail.com>
2021-02-24 16:32:00 +0000

    Adding path in server files.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
-const express = require("express")
+const express = require('express')
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static.apply(path.resolve(__dirname, '../client/build')));
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+app.get('/api', (req, res) => {
+    res.json({ message: 'Hello from server!' });
 });
 
 app.get('*', (req, res) => {
