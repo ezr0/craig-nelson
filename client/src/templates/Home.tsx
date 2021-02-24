64852d3d1fd13ab0eda7ce76808280708ce9ddff
Craig <craignelson88@googlemail.com>
2021-02-24 14:17:04 +0000

    Deploy app to Heroku!

---
+++ client/src/templates/Home.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+const Home = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch("/api")
+            .then(res => res.json())
+            .then(data => setData(data.message));
+    }, []);
+
+    return (
+        <h1>{!data ? "Loading..." : data}</h1>
+    )
+}
+
+export default Home;
